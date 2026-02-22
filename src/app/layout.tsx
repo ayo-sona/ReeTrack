d628941d8f1a9d819a16104a185369bd700a3341
ayo-sona <ayo-sona@users.noreply.github.com>
2026-02-22 20:08:09 +0100

    chore: added google analytics tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Providers } from "../providers/Providers";
 import { Toaster } from "sonner";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
   title: "ReeTrack Inc",
@@ -20,6 +21,7 @@ export default function RootLayout({
           {children}
           <Toaster position="top-right" />
         </Providers>
+        <GoogleAnalytics gaId="G-NFCEF78QJN" />
       </body>
     </html>
   );
