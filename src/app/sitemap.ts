5fd73e252f6e6fa6d7a2181682c0fd96a78cb3d8
ayo-sona <ayo-sona@users.noreply.github.com>
2026-02-22 19:37:57 +0100

    chore: google stuff

---
+++ src/app/sitemap.ts
@@ -0,0 +1,7 @@
+export default function sitemap() {
+    return [
+      { url: "https://yourdomain.com", lastModified: new Date() },
+      { url: "https://yourdomain.com/auth/login", lastModified: new Date() },
+      // add your public pages
+    ];
+  }
